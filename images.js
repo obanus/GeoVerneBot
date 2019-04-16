b441091a9c1e1fddb482c0d6a4fca3149a4f03b2
obanus <olivier.banus@gmail.com>
2019-04-16 21:18:19 +0200

    Define array of image filenames + sources

---
+++ images.js
@@ -0,0 +1,100 @@
+var images = [
+    {
+        "file": "Propeller_Island_by_Léon_Benett_18.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_19.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_23.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_24.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_26.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_32.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_34.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_35.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_36.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_38.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_39.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_41.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_42.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_43.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_46.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_48.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_51.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_55.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_60.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_63.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_65.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_67.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_76.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ },
+    {
+        "file": "Propeller_Island_by_Léon_Benett_77.jpg",
+        "source": "Léon Benett. Illustration de « L'Île à hélice », un roman de Jules Verne, 1895."
+ }
+];
+
+module.exports = images;
