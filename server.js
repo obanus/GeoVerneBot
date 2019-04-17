f8170d4f570958a7f3980921b9559f89d13059d4
obanus <olivier.banus@gmail.com>
2019-04-18 01:27:34 +0200

    add dependency on node-schedule library

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var fs = require('fs'),
     path = require('path'),
+    schedule = require('node-schedule'),
     Twit = require('twit'),
     tracery = require('tracery-grammar'),
     config = require(path.join(__dirname, 'config.js')),
