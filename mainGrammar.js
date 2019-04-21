96c50dc6141424a7aa735eac172a998d448608ee
obanus <olivier.banus@gmail.com>
2019-04-21 22:27:20 +0200

    remove non working text samples, part 2

--- mainGrammar.js
+++ mainGrammar.js
@@ -1,15 +1,15 @@
 var mainGrammar = {
     "origin": [
-"Il est #accroche#, #lieu# qui les #verbe# est #complément# par #détail#, dont #caractéristique# n’est pas #comparateur# #nombre# lieues, suivant #géographe#. \\#GeoVerneBot"],
-    "accroche": ["admis", "assuré", "attesté", "avéré", "certain", "constaté", "déclaré", "démontré", "établi", "évident", "incontestable", "indéniable", "indiscutable", "indubitable", "irrécusable", "irréfragable", "irréfutable", "manifeste", "notoire", "reconnu", "sûr", "vrai"],
+"Il est #accroche#, #lieu# qui les #verbe# est #complément# par #détail#, dont #caractéristique# est #comparateur# #nombre# lieues, suivant #géographe#. \\#GeoVerneBot"],
+    "accroche": ["admis", "assuré", "attesté", "avéré", "certain", "constaté", "démontré", "établi", "évident", "incontestable", "indéniable", "indiscutable", "indubitable", "irrécusable", "irréfragable", "irréfutable", "manifeste", "notoire", "reconnu", "sûr"],
     "lieu": ["l'arche", "l'exsurgence", "la baie", "la barkhane", "la barre", "la butte", "la caldeira", "la calanque", "la chute d'eau", "la colline", "la combe", "la côte", "la crevasse", "la cuesta", "la doline", "la dune", "la falaise", "la forêt ", "la gorge", "la gorge", "la garrigue", "la lagune", "la lagune", "la mer", "la mesa", "la moraine", "la péninsule", "la plage", "la plaine", "la reculée", "la ria", "la rivière", "la rizière", "la sebkha", "la source", "la steppe", "la terrasse", "la tourbière", "la tufière", "la vallée"],
-    "verbe": ["baigne", "jouxte", "borde", "encadre", "entoure", "prolonge", "limite", "longe", "flanque"],
-    "complément": ["circonscrite", "entourée", "enserrée", "encadrée", "ourlée", "enfermée", "ceinturée", "bordée", "enveloppée", "hérissée", "enclavée", "surplombée", "dominée", "surmontée"],
-    "détail": ["une ceinture de récifs coralligènes", "une anse semi-circulaire", "des montagnes couvertes de verdure", "des rios et des étangs peuplés d’alligators verdâtres", "des îlots montagneux très escarpés", "un cordon de récifs et d'îlots désolés", "un port abrité des mauvais vents par un amphithéâtre de montagnes", "des montagnes tapissées de cette herbe envahissante nommée menervia", "des montagnes, dont les cimes pointent au-dessus du dôme des frondaisons"],
-    "caractéristique": ["la circonférence", "la longueur", "la dimension", "la taille", "la largeur", "la profondeur", "l'extension", "le calibre", "l'envergure", "l'étendue", "l'importance", "le développement", "l'empan", "l'élévation", "l'ampleur"],
-    "comparateur": ["inférieure à", "supérieure à"],
-    "nombre": ["cent dix", "cinquante-quatre", "deux cent ", "deux cents", "deux cents", "quatre-vingt-neuf", "six cent cinquante", "soixante et une", "soixante-huit", "trente-huit", "trente-six", "vingt-six"],
-    "géographe": ["Alexander von Humboldt", "Carl Ritter", "Élisée Reclus ", "Friedrich Ratzel", "Paul Vidal de la Blache", "Victor Adolphe Malte-Brun", "William Morris Davis"]
+    "verbe": ["jouxte", "borde", "encadre", "entoure", "prolonge", "limite", "longe", "flanque"],
+    "complément": ["circonscrite", "entourée", "enserrée", "encadrée", "ourlée", "enfermée", "ceinturée", "bordée", "enveloppée", "enclavée", "surplombée", "dominée", "surmontée"],
+    "détail": ["une ceinture de récifs coralligènes", "une anse semi-circulaire", "des montagnes couvertes de verdure", "des étangs peuplés d’alligators verdâtres", "des îlots montagneux très escarpés", "un cordon de récifs et d'îlots désolés", "un port abrité des mauvais vents", "des montagnes"],
+    "caractéristique": ["la circonférence", "la longueur", "la dimension", "la taille", "la largeur", "la profondeur", "l'extension", "l'envergure", "l'étendue", "l'importance", "l'élévation"],
+    "comparateur": ["inférieure à", "supérieure à", "presque égale à", "quasiment égale à", "sinificativement inférieure à"],
+    "nombre": ["cent dix", "cinquante-quatre", "deux cents", "quatre-vingt-neuf", "six cent cinquante", "soixante et une", "soixante-huit", "trente-huit", "trente-six", "vingt-six"],
+    "géographe": ["Alexander von Humboldt", "Carl Ritter", "Élisée Reclus ", "Friedrich Ratzel", "Paul Vidal de la Blache", "Victor Adolphe Malte-Brun", "William Morris Davis", "Mungo Park"]
 };
 
 module.exports = mainGrammar;
